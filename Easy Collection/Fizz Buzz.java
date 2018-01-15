2ac77ab07ede2008da5ee456529a156c2b2adab0
jingwenh <31908198+jingwenh@users.noreply.github.com>
2018-01-15 11:22:12 +0800

    Create Fizz Buzz.java

---
+++ Easy Collection/Fizz Buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    //正常思路，从1数到n，先判断能不能被3和5同时除尽，否则分别判断能否被3或5除尽
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}

a4a5fa6a9ad93bb6aeb67b3eae5d18c1c6cb596a
jingwenh <31908198+jingwenh@users.noreply.github.com>
2018-01-28 12:34:06 -0500

    Create 292. Nim Game.java

---
+++ 292. Nim Game.java
@@ -0,0 +1,7 @@
+//You can always win a Nim game if the number of stones nn in the pile is not divisible by 44.
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}

b7ead81ef8efc1bd9d1c8c5399bfd22f9bb1193e
jingwenh <31908198+jingwenh@users.noreply.github.com>
2018-02-03 17:22:29 -0500

    Update 67. Add Binary.java

--- 67. Add Binary.java
+++ 67. Add Binary.java
@@ -1,21 +1,27 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder res = new StringBuilder();
         while (i >= 0 || j >= 0) {
             int sum = carry;
-            if (j >= 0) {
-                sum = sum + b.charAt(j) - '0';
-                j--;
-            }
             if (i >= 0) {
                 sum = sum + a.charAt(i) - '0';
                 i--;
-            } 
-            sb.append(sum % 2);
+            }
+            if (j >= 0) {
+                sum = sum + b.charAt(j) - '0'; // - '0'把字符转换成数字
+                j--;
+            }
+            res.append(sum % 2 + "");//sum % 2可以得到对应的数位
             carry = sum / 2; //Sum >= 2时carry = 1, Sum < 2时carry = 0
         }
-        if (carry != 0) sb.append(carry);
-        return sb.reverse().toString();
+        
+        //如果最后carry != 0要把carry加上
+        if (carry != 0) {
+            res.append("1");
+        }
+        return new String(res.reverse());
     }
 }

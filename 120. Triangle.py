de53101b44f6ca00b4f5ecb2d62660a61e809ae3
jingwenh <31908198+jingwenh@users.noreply.github.com>
2018-03-11 15:59:16 -0400

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -35,3 +35,30 @@ class Solution:
         if coo[0] > 0 and coo[0] < len(self.triangle) and coo[1] >= 0 and coo[1] <= width:
             return True
         return False
+
+# Solution 2: traverse as a binary tree, TLE
+class Solution:
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        self.min_path = float('inf')
+        self.triangle = triangle
+        self.traverse((0, 0), 0)
+        return self.min_path
+        
+    def traverse(self, root, path_sum):
+        path_sum = path_sum + self.triangle[root[0]][root[1]]
+        if root[0] == len(self.triangle) - 1:
+            self.min_path = min(self.min_path, path_sum)
+        if self.inBound((root[0] + 1, root[1])):
+            self.traverse((root[0] + 1, root[1]), path_sum)
+        if self.inBound((root[0] + 1, root[1] + 1)):
+            self.traverse((root[0] + 1, root[1] + 1), path_sum)
+        
+    def inBound(self, coo):
+        width = coo[0]
+        if coo[0] > 0 and coo[0] < len(self.triangle) and coo[1] >= 0 and coo[1] <= width:
+            return True
+        return False

20f432a071b9946ed6487f4111ad5f27e33a4b52
jingwenh <31908198+jingwenh@users.noreply.github.com>
2018-04-07 23:25:03 -0400

    Update 815. Bus Routes.py

--- 815. Bus Routes.py
+++ 815. Bus Routes.py
@@ -37,3 +37,47 @@ class Solution:
                 new_bus_taken = set(bus_taken)
                 new_bus_taken.add(k)
                 self.takeBus(new_bus_taken, set(v))
+
+    # BFS层序遍历，记录层数，找到T的那一层返回当前层数
+    # 如果没坐过的bus和当前的bus路线有交集就递归，到达终点退出，最后返回最少换乘数量
+    class Solution:
+    def numBusesToDestination(self, routes, S, T):
+        """
+        :type routes: List[List[int]]
+        :type S: int
+        :type T: int
+        :rtype: int
+        """
+        if S == T:
+            return 0
+        
+        self.res = []
+        self.routes_dic = dict()
+        self.T = T
+        self.S = S
+        q = []
+        for i, r in enumerate(routes):
+            self.routes_dic[i] = set(r)
+            if S in r:
+                q.append(i)
+        
+        count = 0
+        bus_taken = set()
+        
+        while len(q) > 0:
+            size = len(q)
+            count = count + 1
+            for i in range(size):
+                cur = q.pop(0)
+                if cur not in bus_taken:
+                    bus_taken.add(cur)
+                else:
+                    continue
+                if T in self.routes_dic[cur]:
+                    self.flag = True
+                    return count                
+                for k, v in self.routes_dic.items():
+                    if len(set.intersection(self.routes_dic[cur], v)) > 0:
+                        q.append(k)
+            
+        return -1
